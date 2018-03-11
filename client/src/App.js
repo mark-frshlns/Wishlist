64419bfce531201f65945bcf9c1f0abe281e4c25
mark7389 <mark.soliman73@gmail.com>
2018-03-10 19:30:14 -0500

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Switch>
+              
+          </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
